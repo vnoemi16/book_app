5ce06dbca18ad64df58c46477f5b808ef4b98abe
Noemi Veress <vnoemi595@gmail.com>
2025-02-22 00:11:05 +0100

    updated auth

--- frontend/src/app/components/auth/signup/signup.component.ts
+++ frontend/src/app/components/auth/signup/signup.component.ts
@@ -2,19 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: '../auth.css'
 })
 export class SignupComponent {
   fb = inject(FormBuilder);
   http = inject(HttpClient);
   authService = inject(AuthService);
+  
   router = inject(Router);
 
   form = this.fb.nonNullable.group({
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable, signal } from "@angular/core";
 import { Auth, user, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, signOut } from "@angular/fire/auth";
 import { Observable, from } from "rxjs";
+import { DataService } from "./data.service";
 
 export enum Role{
     ADMIN = "ADMIN",
@@ -11,7 +12,7 @@ export interface UserInterface {
     email: string;
     username: string;
     id: string;
-    role: Role
+    role: string
 }
 
 @Injectable({
@@ -19,12 +20,16 @@ export interface UserInterface {
 })
 export class AuthService{
     firebaseAuth = inject(Auth);
+    dataService = inject(DataService);
     user$ = user(this.firebaseAuth);
     currentUserSig = signal<UserInterface | null | undefined>(undefined);
 
     signUp(email: string,  username: string, password: string): Observable<void> {
         const promise = createUserWithEmailAndPassword(this.firebaseAuth, email, password)
-        .then(response => updateProfile(response.user, {displayName: username}));
+        .then(response => {
+            updateProfile(response.user, {displayName: username});
+            this.dataService.addUserRole({user_id: response.user.uid, role: "user"});
+        });
         return from(promise);
     }
 
