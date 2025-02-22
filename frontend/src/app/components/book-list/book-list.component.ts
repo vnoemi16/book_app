1ef1999392e07253585c341a75832defbc78c8f5
Noemi Veress <vnoemi595@gmail.com>
2025-02-22 23:55:44 +0100

    moved the search form

--- frontend/src/app/components/book-list/book-list.component.ts
+++ frontend/src/app/components/book-list/book-list.component.ts
@@ -1,43 +1,82 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Observable } from 'rxjs';
-import { BookService } from '../../services/book.service';
+import { DataService } from '../../services/data.service';
 import { Book } from '../../book';
 import { CommonModule } from '@angular/common';
-import { SearchFormComponent } from '../search-form/search-form.component';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { InputBookComponent } from '../input-book/input-book.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MultiDropdownComponent } from '../multi-dropdown/multi-dropdown.component';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule, SearchFormComponent],
+  imports: [CommonModule, InputBookComponent, ReactiveFormsModule, MultiDropdownComponent],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
-export class BookListComponent implements OnInit{
-  bookService = inject(BookService);
+export class BookListComponent implements OnInit {
+  authService = inject(AuthService);
+  dataService = inject(DataService);
   books = signal<Book[]>([]);
+  genreList: Map<string, string> = new Map();
 
-  constructor(private router: Router){
-    this.bookService.getBooks({}).subscribe({
-      next: (data) => {this.books.set(data)},
-      error: (error) => {console.log(error)}
+  addingBook = false;
+  page = 1;
+  maxpage = 1;
+
+  fb = inject(FormBuilder);
+  form = this.fb.nonNullable.group({
+    title: [""],
+    author: [""],
+    genres: [[""]],
+    order: ["neither"],
+    o: [false]
+  });
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.dataService.getBooks({}).subscribe({
+      next: (data) => { this.books.set(data) },
+      error: (error) => { console.log(error) }
+    });
+    this.dataService.getGenres().subscribe((genres) => {
+      this.genreList = new Map(genres.map((item: { genre: any; color: any; }) => [item.genre, item.color]))
     });
   }
 
-  getData(params: any){
-    this.bookService.getBooks(params).subscribe(data => this.books.set(data));
+  getPage(i: number) {
+    if ((i>0 && this.page < this.maxpage) || (i<0 && this.page > 1)){
+      this.page += i;
+    }
+    
   }
 
-  ngOnInit(): void {
-      
-  }
-
-  handleSearch(e: any){
-    console.log(e);
-    this.bookService.getBooks(e).subscribe(data => this.books.set(data));
-  }
-
-  openBook(id: string){
+  openBook(id: string) {
     this.router.navigate(['/book/' + id]);
+  }
+
+  submitBook(e: any) {
+    this.dataService.addBook(e).subscribe(_result => this.dataService.getBooks({}));
+    this.addingBook = false;
+  }
+
+  genresChanged(item: any) {
+    this.form.controls.genres.setValue(item);
+  }
+
+  searchBooks() {
+    this.dataService.getBooks(this.form.getRawValue()).subscribe(data => this.books.set(data));
+  }
+
+  resetSearch() {
+    this.form.controls.author.setValue("");
+    this.form.controls.genres.setValue([]);
+    this.form.controls.title.setValue("");
+    this.form.controls.order.setValue("neither");
+    this.dataService.getBooks({}).subscribe(data => this.books.set(data));
   }
 }
