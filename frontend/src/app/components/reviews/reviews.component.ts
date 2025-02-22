d1b58a1f38f2659a414feb1189755974e827ca48
Noemi Veress <vnoemi595@gmail.com>
2025-02-22 21:26:04 +0100

    added reviews

--- frontend/src/app/components/reviews/reviews.component.ts
+++ frontend/src/app/components/reviews/reviews.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-reviews',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.css'
 })
-export class ReviewsComponent {
+export class ReviewsComponent implements OnInit {
+  @Input() book_id!: string;
+  rating = 0;
+  authService = inject(AuthService);
+  dataService = inject(DataService);
+  averageRating = 0;
+  reviews = signal<any[]>([]);
 
+
+  fb = inject(FormBuilder);
+  form = this.fb.nonNullable.group({
+    review: ['']
+  });
+
+  ngOnInit(): void {
+    this.dataService.getBookAverageRating(this.book_id).subscribe((average) => this.averageRating = average.averageRating);
+    this.dataService.getBookReviews(this.book_id).subscribe((reviews) => this.reviews.set(reviews))
+  }
+
+  rate(rating: number){
+    this.rating = rating;
+    console.log(this.rating);
+  }
+
+  onSubmit() {
+    console.log(this.authService.currentUserSig());
+    const id = this.authService.currentUserSig()?.id;
+    const username = this.authService.currentUserSig()?.username;
+    if (id != null && username != null){
+      const body = {
+        user_id: id,
+        username: username,
+        book_id: this.book_id,
+        stars: this.rating,
+        review: this.form.getRawValue().review
+      }
+      console.log("review sent.");
+      this.dataService.addReview(body).subscribe((_result) => console.log("Review added."));
+    }
+    
+  }
 }
